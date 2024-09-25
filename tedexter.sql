741c38b3eae51d9b8d4a44d968a78c07b54fe401
Federico Medina <smedina@alumnos.exa.unicen.edu.ar>
2024-09-25 08:55:59 -0300

    updated to MVC, changed lists  DB

--- tedexter.sql
+++ tedexter.sql
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clients`
+-- Estructura de tabla para la tabla `client`
 --
 
-CREATE TABLE `clients` (
+CREATE TABLE `client` (
   `id_client` int(20) NOT NULL,
   `name` varchar(40) NOT NULL,
   `email` varchar(30) NOT NULL,
@@ -36,10 +36,10 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `clients`
+-- Volcado de datos para la tabla `client`
 --
 
-INSERT INTO `clients` (`id_client`, `name`, `email`, `addres`, `phone`) VALUES
+INSERT INTO `client` (`id_client`, `name`, `email`, `addres`, `phone`) VALUES
 (1, 'Federico Medina', 'sfmedina@gmail.com', 'Colon 1575', 2494665523),
 (2, 'Giuliana', 'giugmail.com', 'siempre viva 123', 2494007979),
 (6, 'Santino Lanzini', 'sant@yahoo.com.ar', 'Alem 1129', 2494551943);
@@ -47,10 +47,10 @@ INSERT INTO `clients` (`id_client`, `name`, `email`, `addres`, `phone`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `orders`
+-- Estructura de tabla para la tabla `order`
 --
 
-CREATE TABLE `orders` (
+CREATE TABLE `order` (
   `id_order` int(20) NOT NULL,
   `date` date NOT NULL,
   `status` varchar(20) NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE `orders` (
 -- Volcado de datos para la tabla `orders`
 --
 
-INSERT INTO `orders` (`id_order`, `date`, `status`, `id_client`) VALUES
+INSERT INTO `order` (`id_order`, `date`, `status`, `id_client`) VALUES
 (1, '2024-10-10', 'prepared', 1),
 (2, '2024-08-18', 'delayed', 6),
 (3, '2024-09-13', 'shipped', 2);
@@ -73,13 +73,13 @@ INSERT INTO `orders` (`id_order`, `date`, `status`, `id_client`) VALUES
 --
 -- Indices de la tabla `clients`
 --
-ALTER TABLE `clients`
+ALTER TABLE `client`
   ADD PRIMARY KEY (`id_client`);
 
 --
 -- Indices de la tabla `orders`
 --
-ALTER TABLE `orders`
+ALTER TABLE `order`
   ADD PRIMARY KEY (`id_order`),
   ADD KEY `id_client` (`id_client`);
 
@@ -90,13 +90,13 @@ ALTER TABLE `orders`
 --
 -- AUTO_INCREMENT de la tabla `clients`
 --
-ALTER TABLE `clients`
+ALTER TABLE `client`
   MODIFY `id_client` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT de la tabla `orders`
+-- AUTO_INCREMENT de la tabla `order`
 --
-ALTER TABLE `orders`
+ALTER TABLE `order`
   MODIFY `id_order` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
@@ -106,8 +106,8 @@ ALTER TABLE `orders`
 --
 -- Filtros para la tabla `orders`
 --
-ALTER TABLE `orders`
-  ADD CONSTRAINT `id_client` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id_client`);
+ALTER TABLE `order`
+  ADD CONSTRAINT `id_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
