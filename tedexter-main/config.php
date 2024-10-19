a3804c6e30094aef0ee0badf2f04022d7854873d
Federico Medina <smedina@alumnos.exa.unicen.edu.ar>
2024-10-19 16:50:52 -0300

    deleted folders

--- tedexter-main/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-    const MYSQL_USER = 'root';
-    const MYSQL_PASS = '';
-    const MYSQL_DB = 'tedexter';
-    const MYSQL_HOST = 'localhost';
-
-   // $this-> db = new PDO('mysql:host=localhost; dbname=;charset=utf8', 'root','');
